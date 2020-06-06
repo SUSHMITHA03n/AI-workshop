19dd121905832e96fdb1a97e52759720b1d00c90
SUSHMITHA03n <65362207+SUSHMITHA03n@users.noreply.github.com>
2020-06-06 22:35:43 +0530

    Add files via upload

---
+++ positive num.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+l=[]
+c=[]
+n=int(input())
+for i in range(n):
+    x=int(input())
+    l.append(x)
+print(l)
+for i in l:
+    if i>0:
+        c.append(i)
+print(c)
+
+
+# In[ ]:
+
+
+
+
